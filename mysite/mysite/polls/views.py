0ae12126428369bd9027e0455e2b948839de19e1
JP <jaehyung0321@gmail.com>
2021-07-01 01:42:02 +0900

    update_results_html

--- mysite/mysite/polls/views.py
+++ mysite/mysite/polls/views.py
@@ -1,17 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse, response
+from typing import ContextManager
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, response, Http404, HttpResponseRedirect
+from django.urls import reverse
+
+from django.template import loader
+from django.template.context import Context
+
+from .models import Choice, Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi there. I'm your poll index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 항상 리다이렉트로 리턴, 성공적일시
+        # 포스트 데이터 보호, 뒤로가기 버튼을 두번 눌렀을떄
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
